35af9e747c333fb354123d98728b5fea4ba68bb7
rodriguezst <2828844+rodriguezst@users.noreply.github.com>
2022-07-07 13:01:40 +0000

    Deploying to github-pages from @ rodriguezst/nuki_hub@80865523f256384bcfe3f8c5b281a94e41f6468c 🚀

---
+++ flash.sh
@@ -0,0 +1,1 @@
+esptool.py --chip esp32 --port /dev/ttyUSB0 --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode qio --flash_freq 80m --flash_size detect 0xe000 boot_app0.bin 0x1000 bootloader_qio_80m.bin 0x10000 nuki_hub.bin 0x8000 nuki_hub.partitions.bin
